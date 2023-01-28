c9abcc48ee236dfab46c8ff70239c95cd3c279aa
ChewyToast <moll.bruno.99@gmail.com>
2023-01-28 01:24:23 +0100

    verison 0.1

--- minishell_panic.sh
+++ minishell_panic.sh
@@ -9,99 +9,76 @@ BLUE='\033[0;94m'
 MAGENTA='\033[0;95m'
 CYAN='\033[0;96m'
 WHITE='\033[0;97m'
-DARK_YELLOW='\033[0;96m'
+MAIN_COLOR='\033[0;96m'
 
 function echo_test()
 {
-	TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
+	ARGV=$(echo "$@")
+	# echo -n $ARGV
+	FTEST=$(echo "$ARGV" | cut -c 5-20)
+	PRINT=$(echo "$ARGV" | cut -c 1-4)
+	# echo -n "$FTEST"
+	echo -n " "
+	printf ${BLUE};
+	echo -n " "$PRINT
+	printf ${DEF_COLOR};
+	TEST1=$(echo $FTEST | ./minishell 2>&-)
 	ES_1=$?
-	TEST2=$(echo $@ "; exit" | bash 2>&-)
+	TEST1=$(echo "$TEST1" | cat -e)
+	TEST2=$(echo $FTEST | bash 2>&-)
 	ES_2=$?
+	TEST2=$(echo "$TEST2" | cat -e)
 	if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ]; then
-		printf ${GREEN}"OK"
+		printf ${GREEN}"OK";
 	else
-		printf ${RED}"KO"
+		TMP=$(echo "$PRINT" | cut -c 1-2)
+		echo "------------------------- test [$TMP]" >> traces/echo_trace.txt
+		echo "expected: (exit code: $ES_2)" >> traces/echo_trace.txt
+		echo "->$TEST2<-" >> traces/echo_trace.txt
+		echo >> traces/echo_trace.txt
+		echo "found: (exit code: $ES_1)" >> traces/echo_trace.txt
+		echo "->$TES1<-" >> traces/echo_trace.txt
+		echo >> traces/echo_trace.txt
+		echo "-------------------------" >> traces/echo_trace.txt
+		echo >> traces/echo_trace.txt
+		printf ${RED}"KO";
 	fi
-	# printf "$CYAN \"$@\" $RESET"
-	# if [ "$TEST1" != "$TEST2" ]; then
-	# 	echo
-	# 	echo
-	# 	printf $BOLDRED"Your output : \n%.20s\n$BOLDRED$TEST1\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
-	# 	printf $BOLDGREEN"Expected output : \n%.20s\n$BOLDGREEN$TEST2\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
-	# fi
-	# if [ "$ES_1" != "$ES_2" ]; then
-	# 	echo
-	# 	echo
-	# 	printf $BOLDRED"Your exit status : $BOLDRED$ES_1$RESET\n"
-	# 	printf $BOLDGREEN"Expected exit status : $BOLDGREEN$ES_2$RESET\n"
-	# fi
-	# echo
-	sleep 0.1
+	printf ${BLUE};
+	echo -n "]"
+	printf ${DEF_COLOR};
+	sleep 0.05
 }
 
-function cd_test()
-{
-	TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
-	ES_1=$?
-	TEST2=$(echo $@ "; exit" | bash 2>&-)
-	ES_2=$?
-	if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ]; then
-		printf " $BOLDGREEN%s$RESET" "✓ "
-	else
-		printf " $BOLDRED%s$RESET" "✗ "
-	fi
-	printf "$CYAN \"$@\" $RESET"
-	if [ "$TEST1" != "$TEST2" ]; then
-		echo
-		echo
-		printf $BOLDRED"Your output : \n%.20s\n$BOLDRED$TEST1\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
-		printf $BOLDGREEN"Expected output : \n%.20s\n$BOLDGREEN$TEST2\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
-	fi
-	if [ "$ES_1" != "$ES_2" ]; then
-		echo
-		echo
-		printf $BOLDRED"Your exit status : $BOLDRED$ES_1$RESET\n"
-		printf $BOLDGREEN"Expected exit status : $BOLDGREEN$ES_2$RESET\n"
-	fi
-	echo
-	sleep 0.1
-}
+printf ${MAIN_COLOR}"\t\t\t    -----------------------"${DEF_COLOR};
+printf ${MAIN_COLOR}"\n\t\t\t   | 👹 MINISHELL PANIC 👹 |\n"${DEF_COLOR};
+printf ${MAIN_COLOR}"\t\t\t    -----------------------\n\n"${DEF_COLOR};
 
-
-printf ${DARK_YELLOW}"\n\t ****      **** /** /****     ** /**   ******** /**      ** /******** /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /**/**   **/** /** /**/**   /** /**  **//////  /**     /** /**/////  /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /**//** ** /** /** /**//**  /** /** /**        /**     /** /**       /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /** //***  /** /** /** //** /** /** /********* /********** /*******  /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /**  //*   /** /** /**  //**/** /** ////////** /**//////** /**////   /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /**   /    /** /** /**   //**** /**        /** /**     /** /**       /**       /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t /**        /** /** /**    //*** /**  ********  /**     /** /******** /******** /********\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t //         //  //  //      ///  //  ////////   //      //  ////////  ////////  ////////\n\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t    *******     **     /****     ** /**    ******\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /**////**   ****    /**/**   /** /**   **////**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /**   /**  **//**   /**//**  /** /**  **    //\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /*******  **  //**  /** //** /** /** /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /**////  ********** /**  //**/** /** /**\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /**     /**//////** /**   //**** /** //**    **\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   /**     /**     /** /**    //*** /**  //******\n"${DEF_COLOR};
-printf ${DARK_YELLOW}"\t\t\t   //      //      //  //      ///  //    //////\n\n"${DEF_COLOR};
-
-make re -C ../ > /dev/null
+make -C ../
 cp ../minishell .
 chmod 755 minishell
 
 # ECHO TESTS
-printf ${BLUE}"\n\t\t=======================\n"${DEF_COLOR}
-printf ${BLUE}"\nECHO TEST\n\n"${DEF_COLOR}
-printf ${BLUE}"1["${DEF_COLOR}
-echo_test 'echo test tout'
-printf ${BLUE}"]\t"${DEF_COLOR}
-printf ${BLUE}"2["${DEF_COLOR}
-echo_test 'echo test      tout'
-printf ${BLUE}"]\t"${DEF_COLOR}
-printf ${BLUE}"3["${DEF_COLOR}
-echo_test 'echo -n test tout'
-printf ${BLUE}"]\t"${DEF_COLOR}
-printf ${BLUE}"4["${DEF_COLOR}
-echo_test 'echo -n -n -n test tout'
-printf ${BLUE}"]\t"${DEF_COLOR}
-printf ${BLUE}"\n\n\t\t=======================\n"${DEF_COLOR}
+printf ${BLUE}"\n|===============================[ ECHO TESTS ]================================|\n\n"${DEF_COLOR}
+mkdir traces
+touch traces/echo_trace.txt
+echo > traces/echo_trace.txt
+echo_test ' 1.[echo ""     '
+echo_test ' 2.[echo'
+echo_test ' 3.[echO ""    '
+echo_test ' 4.[    echo'
+echo_test ' 5.[    echo       " hi"'
+echo_test ' 6.["echo" ""   '
+echo_test ' 7.[echo hi   '
+echo_test ' 8.[EcHo hi   '
+printf "\n\n"
+echo_test ' 9.[echo """ ""hi" " """'
+echo_test '10.[echo "'""'"'
+echo_test '11.[echo \"hi\"'
+echo_test '12.[echo $HOME'
+echo_test '13.[echo $PATH'
+echo_test '14.[echo $NONEXIST'
+echo_test '15.["      echo"'
+echo_test '16.[\ echo "   " $'
+printf ${BLUE}"\n\n|===============================================================================|\n"${DEF_COLOR}
+
+# PID=$(ps | grep minishell | grep -v "minishell_panic" | awk '{print $1}')
