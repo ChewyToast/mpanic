2a0f7194117b1071148863b5019d2fcd98fd4c0c
ChewyToast <moll.bruno.99@gmail.com>
2023-02-24 14:22:24 +0100

    starting using whiles to exec tests

--- mpanic.sh
+++ mpanic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # colors
 	DEF_COLOR='\033[0;39m'
 	GRAY='\033[0;90m'
@@ -13,40 +14,39 @@
 	MAIN_COLOR='\033[0;96m'
 #
 
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  UTILS
 
-function	clean_exit()
-{
-	rm -rf .errors
-	rm -rf .tmp
-	rm -rf minishell
-	exit
-}
+	function	clean_exit()
+	{
+		rm -rf .errors
+		rm -rf .tmp
+		rm -rf minishell
+		exit
+	}
 
-function	echo_color()
-{
-	printf $1
-	if [ "$3" == "-n" ]; then
-		echo -n $2
-	else
-		echo $2
-	fi
-	printf ${DEF_COLOR}	
-}
+	function	print_color()
+	{
+		printf "$1"
+		printf "$2"
+		printf ${DEF_COLOR}	
+	}
+#
+
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  PREPARE & COMPILATION
 
 	mkdir .errors &> /dev/null
 	mkdir .tmp &> /dev/null
-	echo_color ${GRAY} "Compiling Makefile ... Please wait!"
+	print_color ${GRAY} "Compiling Makefile ... Please wait!\n"
 	MKFF=$(make -C ../ &> .errors/error.txt)
-	echo_color ${GRAY} "Thanks for waiting"
+	print_color ${GRAY} "Thanks for waiting\n"
 	MK_1=$?
 	MKFF=$(cat .errors/error.txt | cut -c 1-46)
 
 	if [ "$MK_1" != "0" ]; then
 		if [ "$MKFF" == "make: *** No targets specified and no makefile" ]; then
-			echo_color ${RED} "Makefile not found!"
+			print_color ${RED} "Makefile not found!\n"
 			echo "Remember to clone it and run as follows:"
 			echo "  1. cd minishell_project_folder"
 			echo "  2. git clone git@github.com:ChewyToast/minishell_panic.git"
@@ -55,7 +55,7 @@ function	echo_color()
 			echo ""
 			clean_exit
 		else
-			echo_color ${RED} "Compilation error"
+			print_color ${RED} "Compilation error\n"
 			echo "-------------"
 			echo "$MKFF"
 			echo "-------------"
@@ -69,26 +69,25 @@ function	echo_color()
 	echo "exit" > .tmp/exec_read.txt
 	< .tmp/exec_read.txt ./minishell &> .tmp/start.txt
 
-# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#
+
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  PREPARE VARIABLES
-# PROMPT
-	prmp=$(cat -e .tmp/start.txt | sed -e "2d")
-	size_prom_cat_exit=${#prmp}
-	size_prom=$(($size_prom_cat_exit - 5))
-	PRMP=$(echo "$prmp" | cut -c 1-$size_prom)
-# TEST COUNTER
-	i=0
-# TEST RESULT
-	ret=0
-# TEST STATUS
-	EOK="OK"
+	# PROMPT
+		prmp=$(cat -e .tmp/start.txt | sed -e "2d")
+		size_prom_cat_exit=${#prmp}
+		size_prom=$(($size_prom_cat_exit - 5))
+		PRMP=$(echo "$prmp" | cut -c 1-$size_prom)
+	# TEST COUNTER
+		i=0
+	# TEST RESULT
+		ret=0
+	# TEST STATUS
+		EOK="OK"
+#
 
 
-# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  TESTS FUNCTIONS
-
-# # >>>> TESTS ECHO
-
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  TESTS FUNCTIONS
 
 	function trace_printer()
 	{
@@ -108,36 +107,44 @@ function	echo_color()
 		echo >> traces/echo_trace.txt
 	}
 
-
 	function print_test_result()
 	{
-		echo_color ${BLUE} "  ${i}.  [" -n
+		if [ "${#i}" == "1" ]; then
+			print_color ${BLUE} "  ${i}.  |"
+		else
+			if [ "${#i}" == "2" ]; then
+				print_color ${BLUE} "  ${i}. |"
+			else
+				if [ "${#i}" == "3" ]; then
+					print_color ${BLUE} "  ${i}.|"
+				fi
+			fi
+		fi
 		if [ "$ret" == 1 ]; then
 			printf "✅"
 		else
 			printf "❌"
 		fi
-		echo_color ${BLUE} " ] ->" -n
+		print_color ${BLUE} "| - |"
 		printf ${MAGENTA};
 		if [ "$2" ]; then
-			echo -n "$2";
+			printf "$2";
 		else
-			echo -n "$1";
+			printf "$1";
 		fi
-		printf "${BLUE}<-${DEF_COLOR}\n\n";
+		printf "${BLUE}|${DEF_COLOR}\n";
 
 	}
 	
-	function echo_simple_test()
+	function one_line_test()
 	{
 		# Declaramos variables para funcion
 		let "i=i+1"
 		FTEST=$(echo "$1")
 
 		# Preparamos archivo que va a ser el input con los argumentos
-		# Print para completar visualizer
- 		echo "$FTEST" > .tmp/exec_read.txt
- 		echo "exit" >> .tmp/exec_read.txt
+		echo "$FTEST" > .tmp/exec_read.txt
+		echo "exit" >> .tmp/exec_read.txt
 
 		# Ejecutamos minishell y bash con mismos comandos y recojemos ES
 		< .tmp/exec_read.txt ./minishell &> .tmp/exec_outp.txt
@@ -154,11 +161,9 @@ function	echo_color()
 			TEST2=$(cat -e .tmp/bash_outp.txt)
 		else
 			if [ "$WCTEST1" == "       3" ]; then
-				# TEST1=$(cat -e .tmp/exec_outp.txt)
 				TEST1=$(cat -e .tmp/exec_outp.txt | sed -e "$ d" | sed -e "1d" | rev )
 				TEST1=${TEST1:$size_prom_cat_exit:${#TEST1}}
 				TEST1=$(echo "$TEST1" | rev )
-				# TEST1=${TEST1:0:7}
 				TEST2=$(cat -e .tmp/bash_outp.txt)
 			else
 				TEST1=$(cat -e .tmp/exec_outp.txt)
@@ -167,8 +172,14 @@ function	echo_color()
 		fi
 
 		# Realizamos comparativas y imprimimos resultado y/o resultado en archivo
-		if [[ "$ES2" == "127" && "$ES1" == "127" && $TEST1 == *"command not found"* ]]; then
-			ret=1;
+		if [[ "$ES2" != "0" && "$ES1" != "0" ]]; then
+			TEST2=${TEST2:18:${#TEST2}}
+			if [[ $TEST1 == *"$TEST2"*  ]]; then
+				ret=1;
+			else
+				ret=0
+				trace_printer "$1" "$i" "$FTEST" "$ES2" "$(cat -e .tmp/bash_outp.txt)" "$ES1" "$TEST1";
+			fi
 		else
 			if [ "$TEST1" == "$TEST2" ] && [ "$ES1" == "$ES2" ]; then
 				ret=1
@@ -177,58 +188,12 @@ function	echo_color()
 				trace_printer "$1" "$i" "$FTEST" "$ES2" "$TEST2" "$ES1" "$TEST1";
 			fi
 		fi
-		PRINT=$(print_test_result "${FTEST}")
-		echo $PRINT
- 		echo "" > .tmp/exec_outp.txt
- 		echo "" > .tmp/bash_outp.txt
- 	}
-
-	function echo_mix_test()
-	{
-		# Preparamos archivo que va a ser el input con los argumentos
-		FTEST=$(< .tmp/exec_read.txt cat | sed -e "$ d")
-		echo "exit" >> .tmp/exec_read.txt
-	
-		# Ejecutamos minishell y bash con mismos comandos y recojemos ES
-		< .tmp/exec_read.txt ./minishell &> .tmp/exec_outp.txt
-		# ES1=$?
-		ES1="not checked"
-		< .tmp/exec_read.txt bash &> .tmp/bash_outp.txt
-		ES2=$?
-
-		# Damos valor a variables para comparar, leemos de los archivos de salida
-		TEST1=$(cat -e .tmp/exec_outp.txt | sed -e "1d" -e "$ d" | sed -e "1d" | sed -e "$ d")
-		TEST2=$(cat -e .tmp/bash_outp.txt)
-		# TEST1=$(cat -e .tmp/exec_outp.txt | sed -e "1d" -e "2d" -e "$ d" | sed -e "$ d")
-
-		# Realizamos comparativas y imprimimos resultado y/o resultado en archivo
-		if [ "$TEST2" == *"command not found"* ] && [ "$TEST1" == *"command not found"* ]; then
-			printf "✅";
-		else
-			if [ "$TEST1" == "$TEST2" ]; then
-				printf "✅";
-			else
-				tracse_printer "$PRINT" "$1" "$FTEST" "$ES1" "$TEST2" "$ES1" "$TEST1";
-				EOK="KO"
-			fi
-		fi
-		printf ${BLUE};
-		echo -n "]";
-		printf " ->";
-		printf ${MAGENTA};
-		if [ "$3" ]; then
-			echo -n "$3";
-		else
-			echo -n "$2";
-		fi
-		printf "${BLUE}<-${DEF_COLOR}\n\n";
+		print_test_result "${FTEST}";
 		echo "" > .tmp/exec_outp.txt
 		echo "" > .tmp/bash_outp.txt
 	}
 
-# >>>>
-
-# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  BANNER
@@ -239,171 +204,155 @@ function	echo_color()
 	printf ${MAIN_COLOR}"\t\t -----------------------\n\n"${DEF_COLOR};
 
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  TEST CALLS
 
-#ECHO
-	printf ${BLUE}"\n|=======================[ ECHO ]=======================|"${DEF_COLOR}
-	rm -rf traces/echo_trace.txt &> /dev/null
-	echo "" > traces/echo_trace.txt
-	echo "**************************************************************" >> traces/echo_trace.txt
-	echo "*                          CARE!                             *" >> traces/echo_trace.txt
-	echo "*                                                            * " >> traces/echo_trace.txt
-	echo "* This test doesnt work if ur readline prompt have newlines  *" >> traces/echo_trace.txt
-	echo "*                                                            *" >> traces/echo_trace.txt
-	echo "**************************************************************" >> traces/echo_trace.txt
-	echo "" >> traces/echo_trace.txt
-	printf ${BLUE}"\n\n\n"${DEF_COLOR}
-	printf "${BLUE} parsing\n ------------------------------------\n\n${DEF_COLOR}"
-	echo_simple_test 'echo ""     '
- 	echo_simple_test 'echo'
-	echo_simple_test 'echO ""    '
-	echo_simple_test '    echo'
-	echo_simple_test '    echo       " hi"'
-	echo_simple_test '"echo" ""   '
-	echo_simple_test 'echo hi   '
-	echo_simple_test 'EcHo hi   '
-	echo_simple_test 'echo """ ""hi" " """'
-	echo_simple_test "\"echo\" ''"
-	echo_simple_test 'echo \"hi\"'
-	echo_simple_test 'echo $HOME'
-	echo_simple_test '  13. [' 'echo $PATH'
-	echo_simple_test '  14. [' 'echo $NONEXIST'
-	echo_simple_test '  15. [' '" echo"'
-	echo_simple_test '  16. [' '\ echo "   " $'
-	echo_simple_test '  17. [' 'echo hi~'
-	echo_simple_test '  18. [' 'echo ~'
-	echo_simple_test '  19. [' 'echo ~false'
-	echo_simple_test '  20. [' 'echo \~'
-	echo_simple_test '  21. [' 'echo "~"ups'
-	printf "${BLUE}\n flags\n ------------------------------------\n\n${DEF_COLOR}"
-	echo_simple_test '  22. [' 'echo -n'
-	echo_simple_test '  23. [' 'echo -n hi'
-	echo_simple_test '  24. [' ' echo -nn'
-	echo_simple_test '  25. [' 'echo -nn hi'
-	echo_simple_test '  26. [' ' echo --n'
-	echo_simple_test '  27. [' ''"'"'echo'"'"' --n hi'
-	echo_simple_test '  28. [' 'echo -n -n'
-	echo_simple_test '  29. [' 'echo -n -n hi'
-	echo_simple_test '  30. [' 'echo -nn -nn'
-	echo_simple_test '  31. [' 'echo -nn -nn hi'
-	echo_simple_test '  32. [' 'echo -n -n -n -n'
-	echo_simple_test '  33. [' 'echo -n -n -n -n hi'
-	echo_simple_test '  34. [' 'echo -n -n -n \-n hi'
-	echo_simple_test '  35. [' 'echo -nn hi --n'
-	echo_simple_test '  36. [' 'echo \-nn hi --n'
-	echo_simple_test '  37. [' 'echo -nn hi --n'
-	echo_simple_test '  38. [' 'echo "-nn" hi -n'
-	echo_simple_test '  39. [' 'echo -------------nnnnnnnnnn hi'
-	echo_simple_test '  40. [' 'echo "-------------nnnnnnnnnn" hi'
-	printf "${BLUE}\n combo\n ------------------------------------\n\n${DEF_COLOR}"
-	# printf ${BLUE}"\n\n\n              ---------------         [ echo plus ]         ---------------            \n\n  "${DEF_COLOR}
-	export ECMD="echo" &> /dev/null
-	echo_simple_test '  41. [' '$ECMD' ''
-	echo_simple_test '  42. [' '$ECMD "hi"'
-	export ECMD="EchO" &> /dev/null
-	echo_simple_test '  43. [' '$ECMD'
-	export ECMD="EChO" &> /dev/null
-	echo_simple_test '  44. [' '$ECMD "hi"'
-	export ECMD="         echo" &> /dev/null
-	echo_simple_test '  45. [' '$ECMD "hi"'
-	export ECMD="         EcHO       " &> /dev/null
-	echo_simple_test '  46. [' '$ECMD " hi"'
-	printf "${RED}\tmore test comming soon...👹${DEF_COLOR}\n"
-	# printf ${BLUE};
-	# echo -n "  47. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="echo"
-	# $ECMD "hi"' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "47";
-	# printf ${BLUE};
-	# echo -n "  48. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="EchO"
-	# $ECMD " hi"' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "48";
-	# printf ${BLUE};
-	# echo -n "  49. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="         EcHO       "
-	# $ECMD "hi"' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "49";
-	# printf ${BLUE};
-	# echo -n "  50. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="         EcHO      hi "
-	# $ECMD' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "50";
-	# printf ${BLUE};
-	# echo -n "  51. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="         '"'"'echo'"'"'      hi "
-	# $ECMD' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "51";
-	# printf ${BLUE};
-	# echo -n "  52. ["
-	# printf ${DEF_COLOR};
-	# echo 'export ECMD="         "echo"      hi "
-	# $ECMD' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "52";
-	# printf ${BLUE};
-	# echo -n "  53. ["
-	# printf ${DEF_COLOR};
-	# echo 'export PATH="."
-	# echo hola' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "53";
-	# printf ${BLUE};
-	# echo -n "  54. ["
-	# printf ${DEF_COLOR};
-	# echo 'export PATH="."
-	# echo hola' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "53";
-	# printf ${BLUE};
-	# echo -n "  55. ["
-	# printf ${DEF_COLOR};
-	# echo 'unset PATH
-	# echo $USER*1' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "54";
-	# printf ${BLUE};
-	# echo -n "  56. ["
-	# printf ${DEF_COLOR};
-	# echo 'unset HOME
-	# echo "~"$USER' > .tmp/exec_read.txt
-	# echo "exit" >> .tmp/exec_read.txt
-	# echo_mix_test "55";
-	# unset ECMD
-	if [ "$EOK" == "KO" ];
-	then
-		printf "${CYAN}\n\n  It seems that there are some tests\n"
-		printf "  that have not passed...\n\n"
-		printf "  Failure traces -> traces/echo_traces.txt\n"
-	fi
-	printf "${BLUE}\n|======================================================|\n\n\n${DEF_COLOR}"
-# #
+	#ECHO
+		printf ${BLUE}"\n|=======================[ ECHO ]=======================|"${DEF_COLOR}
+		rm -rf traces/echo_trace.txt &> /dev/null
 
-# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+		# PRINT IN TRACES
+			echo "" > traces/echo_trace.txt
+			echo "**************************************************************" >> traces/echo_trace.txt
+			echo "*                          CARE!                             *" >> traces/echo_trace.txt
+			echo "*                                                            * " >> traces/echo_trace.txt
+			echo "* This test doesnt work if ur readline prompt have newlines  *" >> traces/echo_trace.txt
+			echo "*                                                            *" >> traces/echo_trace.txt
+			echo "**************************************************************" >> traces/echo_trace.txt
+			echo "" >> traces/echo_trace.txt
+			printf ${BLUE}"\n\n\n"${DEF_COLOR}
+		#
 
-# # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  ENDER
+		# SIMPLE TEST ECHO WHILE
+			printf "${BLUE} simple\n ------------------------------------\n\n${DEF_COLOR}"
+			test_file="./test/echo/echo_tests.txt"
+			while IFS= read -r linea
+			do
+				one_line_test "$linea"
+			done < "$test_file"
+		#
+
+		# FLAGS TEST ECHO WHILE
+			printf "${BLUE}\n flags\n ------------------------------------\n\n${DEF_COLOR}"
+			test_file="./test/echo/echo_flag_tests.txt"
+			while IFS= read -r linea
+			do
+				one_line_test "$linea"
+			done < "$test_file"
+		#
+
+		# # MIXED TEST ECHO WHILE
+		# 	printf "${BLUE} mixed\n ------------------------------------\n\n${DEF_COLOR}"
+		# 	test_file="./test/echo/echo_mix_tests.txt"
+		# 	while IFS= read -r linea
+		# 	do
+		# 		one_line_test "$linea"
+		# 	done < "$test_file"
+		# #
+
+
+		printf "${BLUE}\n combo\n ------------------------------------\n\n${DEF_COLOR}"
+		# printf ${BLUE}"\n\n\n              ---------------         [ echo plus ]         ---------------            \n\n  "${DEF_COLOR}
+		export ECMD="echo" &> /dev/null
+		one_line_test '$ECMD' ''
+		one_line_test '$ECMD "hi"'
+		export ECMD="EchO" &> /dev/null
+		one_line_test '$ECMD'
+		export ECMD="EChO" &> /dev/null
+		one_line_test '$ECMD "hi"'
+		export ECMD="         echo" &> /dev/null
+		one_line_test '$ECMD "hi"'
+		export ECMD="         EcHO       " &> /dev/null
+		one_line_test '$ECMD " hi"'
+		printf "${RED}\n\n\tmore test comming soon...👹${DEF_COLOR}\n"
+		# printf ${BLUE};
+		# echo -n "  47. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="echo"
+		# $ECMD "hi"' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "47";
+		# printf ${BLUE};
+		# echo -n "  48. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="EchO"
+		# $ECMD " hi"' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "48";
+		# printf ${BLUE};
+		# echo -n "  49. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="         EcHO       "
+		# $ECMD "hi"' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "49";
+		# printf ${BLUE};
+		# echo -n "  50. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="         EcHO      hi "
+		# $ECMD' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "50";
+		# printf ${BLUE};
+		# echo -n "  51. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="         '"'"'echo'"'"'      hi "
+		# $ECMD' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "51";
+		# printf ${BLUE};
+		# echo -n "  52. ["
+		# printf ${DEF_COLOR};
+		# echo 'export ECMD="         "echo"      hi "
+		# $ECMD' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "52";
+		# printf ${BLUE};
+		# echo -n "  53. ["
+		# printf ${DEF_COLOR};
+		# echo 'export PATH="."
+		# echo hola' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "53";
+		# printf ${BLUE};
+		# echo -n "  54. ["
+		# printf ${DEF_COLOR};
+		# echo 'export PATH="."
+		# echo hola' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "53";
+		# printf ${BLUE};
+		# echo -n "  55. ["
+		# printf ${DEF_COLOR};
+		# echo 'unset PATH
+		# echo $USER*1' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "54";
+		# printf ${BLUE};
+		# echo -n "  56. ["
+		# printf ${DEF_COLOR};
+		# echo 'unset HOME
+		# echo "~"$USER' > .tmp/exec_read.txt
+		# echo "exit" >> .tmp/exec_read.txt
+		# echo_mix_test "55";
+		# unset ECMD
+		if [ "$EOK" == "KO" ];
+		then
+			printf "${CYAN}\n\n  It seems that there are some tests\n"
+			printf "  that have not passed...\n\n"
+			printf "  Failure traces -> traces/echo_traces.txt\n"
+		fi
+		printf "${BLUE}\n|======================================================|\n\n\n${DEF_COLOR}"
+	#
+#
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  ENDER
 	printf "${BLUE}  Any issue send via slack bmoll-pe, arebelo or ailopez-o\n\n${DEF_COLOR}"
 	rm -rf .errors
 	rm -rf .tmp
 	rm -rf minishell
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-	#test_file="/Users/test/Documents/03_minishell/minishell_panic/to_test/bruno.txt"
-	# while IFS= read -r linea
-	# do
-	# 	echo "$linea"
-	# done < "$test_file"
+#
