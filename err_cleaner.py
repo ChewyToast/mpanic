ad305b3c99b9d6c268393e02016a58ba03efbd18
ChewyToast <moll.bruno.99@gmail.com>
2023-03-12 18:23:40 +0100

    fixing some dqoute tests

--- err_cleaner.py
+++ err_cleaner.py
@@ -13,6 +13,10 @@ for line in output.splitlines():
     if match:
         line_num = int(match.group(1))
         error_msg = match.group(2)
+        # Si el mensaje de error comienza con "syntax error near unexpected token",
+        # solo se debe mantener esa parte del mensaje
+        if error_msg.startswith('syntax error near unexpected token'):
+            error_msg = ' '.join(error_msg.split()[0:5])
         if line_num in error_dict and error_msg != error_dict[line_num]:
             # Si ya hay un error registrado para esta línea y es diferente,
             # omitir esta línea
@@ -22,6 +26,8 @@ for line in output.splitlines():
 # Crear la salida limpia
 clean_output = ''
 for line_num, error_msg in error_dict.items():
+    # Quitar la primera parte hasta después de "line x"
+    error_msg = re.sub(r'bash: line \d+: ', '', error_msg)
     clean_output += f'{error_msg}\n'
 
 # Escribir la salida limpia en el archivo
